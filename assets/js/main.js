68c946d8df0578890afe87381277c3377740a8b2
alexshevelev <60080855+alexshevelev1@users.noreply.github.com>
2020-01-20 17:43:45 +0300

    Update main.js
    автоматический запуск видео

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function autoPlayYouTubeModal(){
   trigger.click(function() {
     var theModal = $(this).data( "target" ),
     videoSRC = $(this).attr( "data-theVideo" ), 
-    videoSRCauto = videoSRC+"?autoplay=0" ;
+    videoSRCauto = videoSRC+"?autoplay=1" ;
     $(theModal+' iframe').attr('src', videoSRCauto);
     $(theModal+' button.close').click(function () {
         $(theModal+' iframe').attr('src', videoSRC);
